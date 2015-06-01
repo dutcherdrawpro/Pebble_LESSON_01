47337fa18a73835e7a2a2743bd1fa06d3d60c76c
dutcherdrawpro <dutcherdraw-productions@outlook.com>
2015-06-01 16:38:00 +0200

    Latest version.

--- src/app.js
+++ src/app.js
@@ -9,36 +9,9 @@ var main = new UI.Card({
 
 main.show();
 
-main.on('click', 'up', function(e) {
-  var main = new UI.Card({
-    title: 'Pebble.js',
-    icon: 'images/menu_icon.png',
-    subtitle: 'Hello World! UP',
-    body: 'UP'
-  });
-  
-  main.show();
-  
-});
-
 main.on('click', 'select', function(e) {
   var main = new UI.Card({
-    title: 'Pebble.js',
-    icon: 'images/menu_icon.png',
-    subtitle: 'Hello World! SELECT',
     body: 'SELECT'
-  });
-  
-  main.show();
-  
-});
-
-main.on('click', 'down', function(e) {
-  var main = new UI.Card({
-    title: 'Pebble.js',
-    icon: 'images/menu_icon.png',
-    subtitle: 'Hello World! DOWN',
-    body: 'DOWN'
   });
   
   main.show();
