30e0117eae45cddd55124f73621fc311cb16a90f
dutcherdrawpro <dutcherdraw-productions@outlook.com>
2015-06-09 08:25:27 +0200

    Changed Code!
    Aangepast

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 var UI = require('ui');
+var Vector2 = require('vector2');
 
 var main = new UI.Card({
   title: 'Pebble.js: Hello World',
@@ -6,12 +7,38 @@ var main = new UI.Card({
   body: 'Druk op een knop!'
 });
 
+newFunction(); 
+
 main.show();
 
 main.on('click', 'select', function(e) {
   var main = new UI.Card({
-    body: 'SELECT'
+    body: 'BODY',
+    subtitle: 'SUBTITLE',
+    title: 'TITLE'
   });
+  
+  main.title('TITLE 1');
   
   main.show();
 });
+
+main.on('click', 'up', function(e) {
+  var wind = new UI.Window();
+  
+  var textfield = new UI.Text({
+    position: new Vector2(0, 0),
+    size: new Vector2(144, 168),
+    font: 'gothic-18-bold',
+    text: 'CUSTOM FONT TYPE'
+  });
+  
+  wind.add(textfield);
+  wind.show();
+});
+
+//main.title('test');
+
+function newFunction(){
+  console.log('This is my function');
+}
